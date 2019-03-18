b83c2b3da69db15e8809fb87552106072d73558e
Peter Orum <peter@peterorum.com>
2019-03-18 16:26:59 +1100

    initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import 'sanitize.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Helo</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
